a24ee376967d1d61c376e9579bb2ba422dd68cf9
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-08-04 18:56:17 +0200

    añadir la función de subir nuevos posts

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import { ui } from "./ui";
 // Obtener posts cuando cargue el DOM
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Evento para añadir un post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Obtener posts
 function getPosts() {
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+}
+
+// Enviar post
+function submitPost(post) {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+    
+    const data = {
+        title,
+        body
+    }
+
+    // Crear post
+    http.post('http://localhost:3000/posts', data)
+        .then(data => {
+            getPosts();
+        })
         .catch(err => console.log(err));
 }
--- src/ui.js
+++ src/ui.js
@@ -16,7 +16,7 @@ class UI {
                 <div class="card mb-3">
                     <div class="card-body">
                         <h4 class="card-title">${post.title}</h4>
-                        <p class="card-text">${post.body}</p>
+                        <p class="card-text lead">${post.body}</p>
                         <a href="#" class="edit card-link" data-id="${post.id}"><i class="fa fa-pencil"></i></a>
                         <a href="#" class="delete card-link" data-id="${post.id}"><i class="fa fa-remove"></i></a>
                     </div>
@@ -24,7 +24,8 @@ class UI {
             `;
         });
 
-        this.posts.insertAdjacentHTML('beforeend', output);
+        // this.posts.insertAdjacentHTML('beforeend', output);
+        this.posts.innerHTML = output;
     }
 }
 
